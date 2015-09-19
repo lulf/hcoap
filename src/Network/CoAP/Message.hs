d568b80f2915e62e533cb3a520a4212586c41839
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2015-09-19 19:56:16 +0200

    Define message datatype

--- src/Network/CoAP/Message.hs
+++ src/Network/CoAP/Message.hs
@@ -1,12 +1,25 @@
 module Network.CoAP.Message
-(
-  Message
+( Message
+, MessageType
+, parseMessage
+, encodeMessage
 ) where
+
+import Data.ByteString
+import Data.Word
+
+data MessageType = Confirmable | NonConfirmable | Acknowledgement | Reset
 
 data Message = Message
   { messageVersion     :: Int
-  , messageType        :: Int
+  , messageType        :: MessageType
   , messageTokenLength :: Int
-  , messageCode        :: Int
-  , messageId          :: Int
+  , messageCode        :: Word8
+  , messageId          :: Word16
   }
+
+parseMessage :: ByteString -> Maybe Message
+parseMessage _ = Nothing
+
+encodeMessage :: Message -> ByteString
+encodeMessage _ = empty
