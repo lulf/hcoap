24e18af788054a9168fc90a65ec3a7025e82fd72
Ulf Lilleengen <ulf.lilleengen@nordicsemi.no>
2015-10-09 19:29:32 +0200

    More work on bringing in state monad

--- src/Network/CoAP/Messaging.hs
+++ src/Network/CoAP/Messaging.hs
@@ -15,7 +15,7 @@ import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import qualified Network.Socket.ByteString as N
 
 type MessageList = [Message]
-type MessageStore = State MessageList
+type MessageStore = State MessageList Message
 
 queueMessage :: Message -> MessageStore
 queueMessage message = do
@@ -34,26 +34,26 @@ handleRequest :: SockAddr -> Message -> Req.Method -> MessageStore -> Req.Reques
 handleRequest hostAddr message method store =
   createRequest hostAddr message method
 
-handleResponse :: Message -> Res.ResponseCode -> MessageStore -> IO ()
-handleResponse _ _ _ = return ()
+handleResponse :: Message -> Res.ResponseCode -> MessageStore
+handleResponse _ _ _ = error "Hey"
 
-handleEmpty :: Message -> MessageStore -> IO ()
-handleEmpty message store = return ()
+handleEmpty :: Message -> MessageStore
+handleEmpty message store = error "hey"
 
 recvRequest :: Socket -> MessageStore -> IO (Req.Request)
 recvRequest sock store = do
   (msgData, hostAddr) <- N.recvFrom sock 65535
   let message = decode msgData
-  let header = messageHeader message
-  let code   = messageCode header
-  return (case code of
-         Request method -> handleRequest hostAddr message method store
-         Response responseCode -> do
-           handleResponse message responseCode store
-           recvRequest sock store
-         Empty -> do
-           handleEmpty message store
-           recvRequest sock store)
+  let header  = messageHeader message
+  let code    = messageCode header
+  case code of
+    Request method -> handleRequest hostAddr message method store
+    Response responseCode -> do
+      --let x = handleResponse message responseCode store
+      recvRequest sock store
+    Empty -> do
+      -- handleEmpty message store
+      recvRequest sock store
 
 sendResponse :: Socket -> MessageStore -> Res.Response -> IO ()
 sendResponse sock store response = return ()
