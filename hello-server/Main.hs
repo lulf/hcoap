52fddbed5e8c7e52057f82d5fd4483f8b3d83956
Ulf Lilleengen <ulf.lilleengen@nordicsemi.no>
2016-01-10 20:03:40 +0100

    Echo request path

--- hello-server/Main.hs
+++ hello-server/Main.hs
@@ -4,12 +4,20 @@ import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.Char8 as L
 import Data.Binary
 
+findPath :: [Option] -> B.ByteString
+findPath [] = B.empty
+findPath (option:options) =
+  case option of
+    UriPath value -> value
+    _             -> findPath options
+
 requestHandler :: Request -> IO Response
-requestHandler req = do
+requestHandler req@(Request id method options payload origin) = do
+  let path = findPath options
   let response = Response { request = req
                           , responseCode = Content
                           , responseOptions = [ContentFormat ApplicationJson]
-                          , responsePayload = Just (B.pack "{\"foo\":42}") }
+                          , responsePayload = Just (B.pack ("{\"path\":\"" ++ (B.unpack path) ++ "\"}")) }
   return response
               
 
