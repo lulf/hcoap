3092dcba101292c2643657521ba2cec154fcf5ea
Ulf Lilleengen <ulf_lilleengen@yahoo.com>
2016-01-13 20:48:51 +0100

    Add quickcheck test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,20 +1,17 @@
 import Network.CoAP.MessageCodec
 import Network.CoAP.Message
-import qualified Network.CoAP.Request as Req
+import Network.CoAP.Request
 import Test.HUnit
+import Data.ByteString
+import Test.QuickCheck
 
 tests = TestList [TestLabel "testEncodeDecode" testEncodeDecode]
-
-main :: IO ()
-main = do
-  runTestTT tests
-  return ()
 
 testEncodeDecode =
   TestCase (do
     let hdr = Header { messageVersion = 1
                      , messageType = CON
-                     , messageCode = Request Req.GET
+                     , messageCode = CodeRequest GET
                      , messageId = 3 }
     let msg = Message { messageHeader = hdr
                       , messageToken = Nothing
@@ -27,3 +24,51 @@ testEncodeDecode =
     assertEqual "Decoded message not same as original" (show msg) (show decoded)
     )
   
+instance Arbitrary Type where
+  arbitrary = elements [CON, NON, ACK, RST]
+
+instance Arbitrary Method where
+  arbitrary = elements [PUT, GET, POST, DELETE]
+
+instance Arbitrary Header where
+  arbitrary = do
+    msgType <- arbitrary
+    msgMethod <- arbitrary
+    msgId <- arbitrary
+    return (Header { messageVersion = 1
+                   , messageType    = msgType
+                   , messageCode    = CodeRequest msgMethod
+                   , messageId      = msgId})
+
+genSafeChar :: Gen Char
+genSafeChar = elements ['a'..'z']
+
+genSafeString :: Gen String
+genSafeString = vectorOf 10 genSafeChar
+
+instance Arbitrary ByteString where
+  arbitrary = do
+    str <- arbitrary
+    return (pack str)
+
+
+instance Arbitrary Message where
+  arbitrary = do
+    hdr <- arbitrary
+    return (Message { messageHeader = hdr
+                    , messageToken = Nothing
+                    , messageOptions = []
+                    , messagePayload = Nothing })
+
+
+checkCodec msg =
+  let encoded = encode msg
+      decoded = decode encoded
+   in (show msg) == (show decoded)
+
+main :: IO ()
+main = do
+  runTestTT tests
+  quickCheck checkCodec
+  return ()
+
